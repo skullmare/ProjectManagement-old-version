7fc723f6fedeee131e4fd783090c147a219051f5
Ivan Ostapchuk <ostapchuk.ivan.2003@gmail.com>
2024-10-09 14:13:37 +0300

    add is_manager role

--- app/projects/views.py
+++ app/projects/views.py
@@ -6,22 +6,23 @@ from rest_framework import serializers
 from .models import Project
 from .serializers import ProjectSerializer
 
-
 class ProjectListCreateView(generics.ListCreateAPIView):
-    queryset = Project.objects.all()
     serializer_class = ProjectSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         user = self.request.user
+        if user.is_manager:
+            return Project.objects.all()  # Менеджер может видеть все проекты
         # Возвращает проекты, в которых пользователь является лидер или участник
         return Project.objects.filter(leaders=user) | Project.objects.filter(participants=user)
 
     def perform_create(self, serializer):
         user = self.request.user
-        if not user.is_leader:
+        if user.is_leader:
+            serializer.save(leaders=[user])  # Устанавливаем текущего пользователя как лидера проекта
+        else:
             raise serializers.ValidationError("У вас недостаточно прав для создания проекта.")
-        serializer.save(leaders=[user])  # Устанавливаем текущего пользователя как лидера проекта
 
 class ProjectRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Project.objects.all()
@@ -30,7 +31,7 @@ class ProjectRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
 
     def retrieve(self, request, *args, **kwargs):
         project = self.get_object()
-        if request.user in project.participants.all() or request.user in project.leaders.all():
+        if request.user in project.participants.all() or request.user in project.leaders.all() or request.user.is_manager:
             serializer = self.get_serializer(project)
             return Response(serializer.data)
         else:
@@ -38,7 +39,7 @@ class ProjectRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
 
     def update(self, request, *args, **kwargs):
         project = self.get_object()
-        if request.user in project.leaders.all():
+        if request.user.is_leader:  # Только лидеры могут обновлять проект
             serializer = self.get_serializer(project, data=request.data, partial=True)
             serializer.is_valid(raise_exception=True)
             self.perform_update(serializer)
@@ -48,7 +49,7 @@ class ProjectRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
 
     def destroy(self, request, *args, **kwargs):
         project = self.get_object()
-        if request.user in project.leaders.all():
+        if request.user.is_leader:  # Только лидеры могут удалять проект
             self.perform_destroy(project)
             return Response(status=status.HTTP_204_NO_CONTENT)
         else:
--- app/user/models.py
+++ app/user/models.py
@@ -28,6 +28,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     first_name = models.CharField(max_length=30)  # Обязательное поле
     last_name = models.CharField(max_length=30)   # Обязательное поле
     is_leader = models.BooleanField(default=False)
+    is_manager = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
